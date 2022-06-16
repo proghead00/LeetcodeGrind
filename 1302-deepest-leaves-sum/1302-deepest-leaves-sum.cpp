7fbb872ab4188fd44b44ef933755f3215f43fea8
Susnata Goswami <55017730+proghead00@users.noreply.github.com>
2022-06-16 19:28:12 +0530

    Time: 278 ms (5.11%), Space: 71.3 MB (5.12%) - LeetHub

--- 1302-deepest-leaves-sum/1302-deepest-leaves-sum.cpp
+++ 1302-deepest-leaves-sum/1302-deepest-leaves-sum.cpp
@@ -1,27 +1,26 @@
 class Solution {
 public:
-  void help(TreeNode *root, int depth, int deepest, int ans,
-            multimap<int, TreeNode *> &mp) {
+  void help(TreeNode *root, int depth, multimap<int, TreeNode *> &mp) {
 
-    if (!root) {
+    if (!root)
       return;
-    }
-    if (!root->left and !root->right) {
+
+    if (!root->left && !root->right)
       mp.insert({depth, root});
-    }
-    help(root->left, depth + 1, deepest, ans, mp);
-    help(root->right, depth + 1, deepest, ans, mp);
+
+    help(root->left, depth + 1, mp);
+    help(root->right, depth + 1, mp);
   }
 
   int deepestLeavesSum(TreeNode *root) {
     int ans = 0;
     multimap<int, TreeNode *> mp;
-    help(root, 0, 0, ans, mp);
+    help(root, 0, mp);
 
     int mx = INT_MIN;
-    for (auto x : mp) {
+    for (auto x : mp)
       mx = max(mx, x.first);
-    }
+
     for (auto x : mp) {
       if (x.first == mx) {
         ans += x.second->val;

47465a9db8c9bbfaebe03e03a970068afb89e51f
Susnata Goswami <55017730+proghead00@users.noreply.github.com>
2022-06-20 12:22:27 +0530

    Time: 228 ms (13.87%), Space: 66.3 MB (44.56%) - LeetHub

--- 820-short-encoding-of-words/820-short-encoding-of-words.cpp
+++ 820-short-encoding-of-words/820-short-encoding-of-words.cpp
@@ -3,12 +3,8 @@ class Node {
 public:
   char data;
   unordered_map<char, Node *> mp;
-  bool isTerminal;
 
-  Node(char d) {
-    data = d;
-    isTerminal = false;
-  }
+  Node(char d) { data = d; }
 };
 
 class Trie {
@@ -20,6 +16,10 @@ public:
     Node *temp = root;
     reverse(word.begin(), word.end());
 
+    // taking a flag so that i can keep track of whether the word is inserted or
+    // ignored
+    // if f=1 in the end, add the length of the word to ans
+
     int f = 0;
     for (char w : word) {
       if (!temp->mp.count(w)) {
@@ -29,7 +29,7 @@ public:
       }
       temp = temp->mp[w];
     }
-    temp->isTerminal = true;
+
     if (f)
       ans += word.size() + 1;
   }
@@ -37,13 +37,18 @@ public:
 
 class Solution {
 public:
+  static bool cmp(string a, string b) { return a.size() > b.size(); }
+
   int minimumLengthEncoding(vector<string> &words) {
 
     Trie t;
-
     int ans = 0;
-    sort(words.begin(), words.end(),
-         [](string a, string b) { return a.size() > b.size(); });
+
+    // "me", time" ==> if i don't sort it, i'll have both "me" and "time"
+    // inserted and eventually would get wrong ans
+    sort(words.begin(), words.end(), cmp);
+    // sort(words.begin(), words.end(),
+    //      [](string a, string b) { return a.size() > b.size(); });
 
     for (string w : words)
       t.insert(w, ans);

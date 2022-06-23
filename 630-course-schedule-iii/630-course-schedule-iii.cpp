fffcf2786cfd27f617c9a301460969f5604178e8
Susnata Goswami <55017730+proghead00@users.noreply.github.com>
2022-06-23 11:03:13 +0530

    Time: 2304 ms (5.09%), Space: 272.4 MB (5.74%) - LeetHub

---
+++ 630-course-schedule-iii/630-course-schedule-iii.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+  int scheduleCourse(vector<vector<int>>& courses) {
+    priority_queue<int> pq;
+
+    sort(courses.begin(), courses.end(), [](auto a, auto b) {
+      return a[1] < b[1];
+    });
+
+    int tt = 0;
+    for (auto c : courses) {
+      if (c[0] <= c[1] and c[0] + tt <= c[1]) {
+        pq.push(c[0]); // c[1] doesn't matter since it will change
+        tt += c[0];
+      }
+      else {
+        if (pq.size() and pq.top() > c[0]) {
+
+          // can swap
+          tt -= pq.top();
+          pq.pop();
+          pq.push(c[0]);
+          tt += c[0];
+        }
+      }
+    }
+
+    return pq.size();
+  }
+};

a9b5ca849a42e4438a4729d63454f13d7921b98d
Susnata Goswami <55017730+proghead00@users.noreply.github.com>
2022-06-24 18:13:33 +0530

    Time: 4 ms (72.67%), Space: 11.5 MB (82.52%) - LeetHub

---
+++ 22-generate-parentheses/22-generate-parentheses.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+
+	void help(int n, string &temp, vector<string> &ans, int open, int close) {
+		if (temp.size() == 2 * n) {
+			if (close == n and open == n) ans.push_back(temp);
+			return;
+		}
+
+		if (open <= n - 1) {
+			temp.push_back('(');
+			help(n, temp, ans, open + 1, close);
+			temp.pop_back();
+
+		}
+
+		if (close < open) {
+			temp.push_back(')');
+			help(n, temp, ans, open, close + 1);
+			temp.pop_back();
+
+		}
+
+	}
+
+	vector<string> generateParenthesis(int n) {
+
+		vector<string> ans;
+		string temp;
+		help(n, temp, ans, 0, 0);
+		return ans;
+	}
+};

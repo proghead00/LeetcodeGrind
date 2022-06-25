016cfd6e7aea5ee8d1dc851fcb116151cb8c8d7d
Susnata Goswami <55017730+proghead00@users.noreply.github.com>
2022-06-25 10:39:50 +0530

    Time: 33 ms (75.54%), Space: 27 MB (53.59%) - LeetHub

--- 665-non-decreasing-array/665-non-decreasing-array.cpp
+++ 665-non-decreasing-array/665-non-decreasing-array.cpp
@@ -2,18 +2,29 @@ class Solution {
 public:
 	bool checkPossibility(vector<int>& nums) {
 		if (nums.size() <= 2) return true;
-		int cnt = 0;
+		bool changed = false;
 
-		for (int i = 1; i < nums.size(); i++) {
-			if (nums[i - 1] > nums[i]) {
-				if (cnt) return false;
-                
-				cnt++;
+		for (int i = 0; i < nums.size() - 1; i++) {
+			if (nums[i] <= nums[i + 1]) continue;
+			// used continue, hence we only have nums[i] > nums[i+1]
 
-				if (i < 2 or nums[i - 2] <= nums[i]) nums[i - 1] = nums[i];
-				else nums[i] = nums[i - 1];
-			}
+			if (changed) return false; // cannot mutate any further (since, atmost one)
+
+			// try to decrease (i-1)th element when standing at i
+			// 	▶ since we don't know what's ahead of ith element yet, and it'd be better if we have smaller element on ith, so that there can be greater elements on the right without any conflict
+
+			//	  i  i+1
+			// 2  5  4
+			if (!i or nums[i + 1] >= nums[i - 1])
+				nums[i] = nums[i + 1];
+
+			//	  i  i+1
+			// 3  5  2
+			else nums[i + 1] = nums[i];
+
+			changed = true;
 		}
+
 		return true;
 	}
 };

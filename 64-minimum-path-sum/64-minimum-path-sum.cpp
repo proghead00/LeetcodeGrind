df2fbd694796ef8cdb0959a806f503a803310489
Susnata Goswami <55017730+proghead00@users.noreply.github.com>
2022-06-28 00:39:19 +0530

    Time: 20 ms (23.00%), Space: 10.1 MB (28.20%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.cpp
+++ 64-minimum-path-sum/64-minimum-path-sum.cpp
@@ -1,29 +1,31 @@
 class Solution {
 public:
-  int help(vector<vector<int>> &grid, int i, int j, vector<vector<int>> &dp) {
+  int help(vector<vector<int>> &grid, int n, int m, vector<vector<int>> &dp) {
 
-    if (!i and !j)
-      return grid[0][0];
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < m; j++) {
+        if (!i and !j)
+          dp[i][j] = grid[i][j];
+          
+        else {
+          int up = i ? dp[i - 1][j] : 1e9;
+          up += grid[i][j];
 
-    if (i < 0 or j < 0)
-      return 1e9;
-
-    // NOTE: BE AWARE OF PLACING ARRAYS AT THE FRONT
-    // WHEN i IS OUTTA BOUNDS, IT'LL THROW AN ERROR
-    // HENCE, PUT DP CHECK AFTER THE ABOVE CONDITIONS
-    if (dp[i][j] != 1e9)
-      return dp[i][j];
-
-    int up = grid[i][j] + help(grid, i - 1, j, dp);
-    int left = grid[i][j] + help(grid, i, j - 1, dp);
-    return dp[i][j] = min(up, left);
+          int left = j ? dp[i][j - 1] : 1e9;
+          left += grid[i][j];
+            
+          dp[i][j] = min(up, left);
+        }
+      }
+    }
+    return dp[n - 1][m - 1];
   }
 
   int minPathSum(vector<vector<int>> &grid) {
     int n = grid.size();
     int m = grid[0].size();
 
-    vector<vector<int>> dp(n, vector<int>(m, 1e9));
-    return help(grid, n - 1, m - 1, dp);
+    vector<vector<int>> dp(n, vector<int>(m, 0));
+    return help(grid, n, m, dp);
   }
 };

f58957e17e6cb0e390dd36eb417647d75ef842e0
Susnata Goswami <55017730+proghead00@users.noreply.github.com>
2022-07-04 21:39:35 +0530

    Time: 6 ms (5.09%), Space: 5.9 MB (84.20%) - LeetHub

--- 779-k-th-symbol-in-grammar/779-k-th-symbol-in-grammar.cpp
+++ 779-k-th-symbol-in-grammar/779-k-th-symbol-in-grammar.cpp
@@ -1,7 +1,15 @@
 class Solution {
 public:
 
-  int kthGrammar(int N, int K) {
-    return __builtin_popcount(K - 1) & 1;
+  int flip(int x) {
+    return x == 0 ? 1 : 0;
+  }
+
+  int kthGrammar(int n, int k) {
+    if (n == 1) return 0;
+
+    if (k & 1) return kthGrammar(n - 1, (k + 1) / 2);
+
+    return flip(kthGrammar(n - 1, k / 2));
   }
 };

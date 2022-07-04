6e6aaea88d28b93d93be7faea317434b8c306f15
Susnata Goswami <55017730+proghead00@users.noreply.github.com>
2022-07-04 13:36:33 +0530

    Time: 218 ms (23.29%), Space: 7.4 MB (64.37%) - LeetHub

--- 486-predict-the-winner/486-predict-the-winner.cpp
+++ 486-predict-the-winner/486-predict-the-winner.cpp
@@ -1,18 +1,32 @@
 class Solution {
 public:
-  bool help(vector<int> &nums, int p, int scoreA, int scoreB, int l, int r) {
+  // only calculating score wrt P1
+  int help(vector<int> &nums, int l, int r) {
     if (l > r)
-      return scoreA >= scoreB;
+      return 0;
 
-    if (p == 1)
-      return help(nums, 2, scoreA + nums[l], scoreB, l + 1, r) or
-             help(nums, 2, scoreA + nums[r], scoreB, l, r - 1);
-    else
-      return help(nums, 1, scoreA, scoreB + nums[l], l + 1, r) and
-             help(nums, 1, scoreA, scoreB + nums[r], l, r - 1);
+    if (l == r)
+      return nums[l];
+
+    // when P1 picks up nums[l], he'll try to minimize P2's score
+    int curScoreL =
+        nums[l] + min(help(nums, l + 2, r), help(nums, l + 1, r - 1));
+
+    int curScoreR =
+        nums[r] + min(help(nums, l, r - 2), help(nums, l + 1, r - 1));
+
+    return max(curScoreL, curScoreR);
   }
-
+    
   bool PredictTheWinner(vector<int> &nums) {
-    return help(nums, 1, 0, 0, 0, nums.size() - 1);
+    int p1Score = help(nums, 0, nums.size() - 1);
+
+    int sum = 0;
+    for (int i = 0; i < nums.size(); i++) {
+      sum += nums[i];
+    }
+
+    // sum - p1Score is P2's score, hence P1 can win iff sum - p1Score >= 0
+    return p1Score >= sum - p1Score;
   }
 };

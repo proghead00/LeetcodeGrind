e006e9aa3267652fedb3e6c46705d45ef468c8f0
Susnata Goswami <55017730+proghead00@users.noreply.github.com>
2022-07-07 21:52:24 +0530

    Time: 164 ms (39.94%), Space: 10.2 MB (78.62%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
@@ -3,31 +3,40 @@ public:
        // rows change simultaneously, so we can keep a single variable for it
        // columns change for both
 
-       int dy[3] = {0, -1, 1};
        int dp[71][71][71];
-       int help(vector<vector<int>> &grid, int i, int r1, int r2) {
+       int help(vector<vector<int>> &grid, int i, int j1, int j2) {
 
-              if (i == grid.size()) return 0;
+              if (j1 < 0 or j1 >= grid[0].size() or j2 < 0 or j2 >= grid[0].size()) return -1e9;
 
-              if (r1 < 0 or r1 >= grid[0].size() or r2 < 0 or r2 >= grid[0].size()) return -1e9;
+              if (i == grid.size() - 1) {
+                     if (j1 == j2) return  dp[i][j1][j2] = grid[i][j1];
+                     else return  dp[i][j1][j2] = grid[i][j1] + grid[i][j2];
+              }
 
               // for every choice of robot1, I have 3 choices of robot2
               // hence, 3 * 3 = 9
 
-              if (dp[i][r1][r2] != -1) return dp[i][r1][r2];
+              if (dp[i][j1][j2] != -1) return dp[i][j1][j2];
 
               int mx = 0;
 
-              for (int outer = 0; outer < 3; outer++) {
-                     for (int inner = 0; inner < 3; inner++) {
-                            mx = max(mx, help(grid, i + 1, r1 + dy[outer], r2 + dy[inner]));
+              // -1, 0, 1 (three directions)
+              for (int outer = -1; outer <= 1; outer++) {
+                     for (int inner = -1; inner <= 1; inner++) {
+
+                            int val = 0;
+
+                            if (j1 == j2) val = grid[i][j1]; // or grid[i][j2]
+                            else val = grid[i][j1] + grid[i][j2];
+
+                            // explore the rest of the grid
+                            val += help(grid, i + 1, j1 + outer, j2 + inner);
+
+                            mx = max(mx, val);
                      }
               }
 
-              if (r1 == r2) mx += grid[i][r1]; // or grid[i][r2]
-              else mx += grid[i][r1] + grid[i][r2];
-
-              return dp[i][r1][r2] = mx;
+              return dp[i][j1][j2] = mx;
        }
 
        int cherryPickup(vector<vector<int>>& grid) {

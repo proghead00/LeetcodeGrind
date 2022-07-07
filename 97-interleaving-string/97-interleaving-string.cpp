a97806c23bdd26992bf808f2806bd4b7e9565df0
Susnata Goswami <55017730+proghead00@users.noreply.github.com>
2022-07-07 13:49:28 +0530

    Time: 56 ms (21.64%), Space: 80.6 MB (5.01%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.cpp
+++ 97-interleaving-string/97-interleaving-string.cpp
@@ -10,8 +10,8 @@ public:
        // 2 -> s1's char matches s3's ==> help(s1, s2, s3, l1-1, l2, l3-1)
        // 3 -> s2's char matches s3's ==> help(s1,s2,s3, l1, l2-1, l3-1)
 
-       int dp[105][105][205];
-       bool help(string &s1, string &s2, string &s3, int l1, int l2, int l3) {
+       // int dp[105][105][205];
+       bool help(string &s1, string &s2, string &s3, int l1, int l2, int l3, vector<vector<vector<int>>> &dp) {
 
               if (l3 < 0 and l1 < 0 and l2 < 0) return true;
 
@@ -20,17 +20,17 @@ public:
               // case 1: pick either of the paths
               if (l1 >= 0 and l2 >= 0 and s1[l1] == s3[l3] and s2[l2] == s3[l3]) {
                      bool first, second;
-                     first = help(s1, s2, s3, l1 - 1, l2, l3 - 1);
-                     second = help(s1, s2, s3, l1, l2 - 1, l3 - 1);
+                     first = help(s1, s2, s3, l1 - 1, l2, l3 - 1, dp);
+                     second = help(s1, s2, s3, l1, l2 - 1, l3 - 1, dp);
                      return dp[l1][l2][l3] = first or second;
               }
               // case 2:
               else if (l1 >= 0 and s1[l1] == s3[l3])
-                     return help(s1, s2, s3, l1 - 1, l2, l3 - 1);
+                     return help(s1, s2, s3, l1 - 1, l2, l3 - 1, dp);
 
               // case 3:
               else if (l2 >= 0 and s2[l2] == s3[l3])
-                     return help(s1, s2, s3, l1, l2 - 1, l3 - 1);
+                     return help(s1, s2, s3, l1, l2 - 1, l3 - 1, dp);
 
               else return false;
        }
@@ -38,10 +38,10 @@ public:
        bool isInterleave(string s1, string s2, string s3) {
 
               int l1 = s1.size(), l2 = s2.size(), l3 = s3.size();
-              // vector<vector<vector<int>>> dp(l1 + 1, vector<vector<int>>(l2 + 1, vector<int>(l3 + 1, -1)));
-              memset(dp, -1, sizeof(dp));
+              vector<vector<vector<int>>> dp(l1 + 1, vector<vector<int>>(l2 + 1, vector<int>(l3 + 1, -1)));
+              // memset(dp, -1, sizeof(dp));
 
               if (l1 + l2 != l3) return false;
-              return help(s1, s2, s3, l1 - 1, l2 - 1, l3 - 1);
+              return help(s1, s2, s3, l1 - 1, l2 - 1, l3 - 1, dp);
        }
 };

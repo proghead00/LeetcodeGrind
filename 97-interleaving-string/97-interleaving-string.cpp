9e70f6037e433f524d0490df9a9556aeeb2c0cb2
Susnata Goswami <55017730+proghead00@users.noreply.github.com>
2022-07-07 13:50:42 +0530

    Time: 69 ms (17.95%), Space: 80.7 MB (5.01%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.cpp
+++ 97-interleaving-string/97-interleaving-string.cpp
@@ -5,12 +5,11 @@ public:
        // cases:
        // NOTE: FIRST CASE SHOULD BE TO PICK EITHER OF THE PATHS
 
-       // 1 -> both s1's and s2's char matches s3's ==> help(s1,s2,s3, l1-1, l2-1, l3-1) ==> pick either of the paths (the best one)
+       // 1 -> both s1's and s2's char matches s3's ==> help(s1,s2,s3, l1-1, l2-1, l3-1) ==> pick either of the paths (the best one, i.e, we'll try to find if we get a true from either of the paths)
 
        // 2 -> s1's char matches s3's ==> help(s1, s2, s3, l1-1, l2, l3-1)
        // 3 -> s2's char matches s3's ==> help(s1,s2,s3, l1, l2-1, l3-1)
 
-       // int dp[105][105][205];
        bool help(string &s1, string &s2, string &s3, int l1, int l2, int l3, vector<vector<vector<int>>> &dp) {
 
               if (l3 < 0 and l1 < 0 and l2 < 0) return true;
@@ -39,7 +38,6 @@ public:
 
               int l1 = s1.size(), l2 = s2.size(), l3 = s3.size();
               vector<vector<vector<int>>> dp(l1 + 1, vector<vector<int>>(l2 + 1, vector<int>(l3 + 1, -1)));
-              // memset(dp, -1, sizeof(dp));
 
               if (l1 + l2 != l3) return false;
               return help(s1, s2, s3, l1 - 1, l2 - 1, l3 - 1, dp);

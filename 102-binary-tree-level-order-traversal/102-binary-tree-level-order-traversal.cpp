725779321534a53b062823fa94494f6187683564
Susnata Goswami <55017730+proghead00@users.noreply.github.com>
2022-07-11 19:25:31 +0530

    Time: 8 ms (42.37%), Space: 12.6 MB (61.86%) - LeetHub

--- 102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.cpp
+++ 102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.cpp
@@ -1,50 +1,37 @@
 class Solution {
 public:
-  vector<vector<int>> levelOrder(TreeNode *root) {
-    vector<vector<int>> res;
-      
-    if (!root)
-      return res;
-      
-    queue<TreeNode *> q;
+	vector<vector<int>> levelOrder(TreeNode* root) {
+		vector<vector<int>> ans;
+		if (!root) return ans;
 
-    q.push(root);
-    q.push(NULL); // NULL acts as a barrier between levels
+		// null denotes barrier
+		queue<TreeNode* > q;
+		q.push(root);
+		q.push(NULL);
 
-    vector<int> v;
-    while (!q.empty()) {
-      TreeNode *temp = q.front();
+		vector<int> temp;
 
-      if (temp) {
-        v.push_back(temp->val);
-        q.pop();
+		while (!q.empty()) {
+			TreeNode* ff = q.front();
+			q.pop();
 
-        // when I am popping out the current parent, I need to add the children to the queue
-          
-        if (temp->left)
-          q.push(temp->left);
-        if (temp->right)
-          q.push(temp->right);
-          
-      } else {
-          
-        // i.e when the node is NULL, I am at the end of a level
-        res.push_back(v); // push the current level to the result
-        v.clear();
-        q.pop(); // pop the barrier (NULL)
-    
-        // CORNER CASE:
-          
-        // let's say the queue is:
-        // 1̶ N̶U̶L̶L̶ 2̶ 3̶ N̶U̶L̶L̶ 4̶ 5̶ 6̶ N̶U̶L̶L̶ 7 NULL
-        // 1̶ N̶U̶L̶L̶ 2̶ 3̶ N̶U̶L̶L̶ 4̶ 5̶ 6̶ N̶U̶L̶L̶ 7̶ NULL
-        // now, we got NULL at front(), BUT WE CANNOT ADD A NEW NULL IN QUEUE
-        // so if we pop out this NULL, the queue becomes empty and we DO NOT add
-        // a new NULL
-        if (!q.empty())
-          q.push(NULL);
-      }
-    }
-    return res;
-  }
+			if (ff) {
+				// I wont push NULL in the vector ofc:: NULL will be inside QUEUE (TreeNode* data type)
+				temp.push_back(ff->val);
+				if (ff->left) q.push(ff->left);
+				if (ff->right) q.push(ff->right);
+			}
+			else {
+				// cur node is NULL, so we push the previous NOT NULLs into final ans
+				ans.push_back(temp);
+				temp.clear();
+
+				// corner case
+				if (!q.empty())
+					q.push(NULL);
+			}
+
+		}
+		return ans;
+	}
 };

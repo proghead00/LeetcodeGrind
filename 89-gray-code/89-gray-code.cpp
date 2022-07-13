5caa4f3f12093d06f27290fab2764a4754dcbe14
Susnata Goswami <55017730+proghead00@users.noreply.github.com>
2022-07-13 12:51:21 +0530

    Time: 206 ms (5.09%), Space: 68.1 MB (5.04%) - LeetHub

--- 89-gray-code/89-gray-code.cpp
+++ 89-gray-code/89-gray-code.cpp
@@ -1,59 +1,48 @@
 class Solution {
 public:
-       int convert(int n) {
-              int dec = 0, i = 0, rem;
-              while (n != 0) {
-                     rem = n % 10;
-                     n /= 10;
-                     dec += rem * pow(2, i);
-                     ++i;
-              }
 
-              return dec;
-       }
+    vector<string> help(int n) {
 
-       vector<string> help(int n) {
+        if (n == 1) {
+            return {"0", "1"};
+        }
 
-              if (n == 1) {
-                     return {"0", "1"};
-              }
+        vector<string> temp = help(n - 1);
 
-              vector<string> temp = help(n - 1);
+        vector<string> zeroesAdd;
+        vector<string> onesAdd;
 
-              vector<string> zeroesAdd;
-              vector<string> onesAdd;
+        for (auto pp : temp) {
+            zeroesAdd.push_back("0" + pp);
+        }
 
-              for (auto pp : temp) {
-                     zeroesAdd.push_back("0" + pp);
-              }
+        reverse(temp.begin(), temp.end());
+        for (auto pp : temp) {
+            onesAdd.push_back("1" + pp);
+        }
 
-              reverse(temp.begin(), temp.end());
-              for (auto pp : temp) {
-                     onesAdd.push_back("1" + pp);
-              }
+        vector<string> merged;
 
-              vector<string> merged;
+        for (auto ff : zeroesAdd)
+            merged.push_back(ff);
 
-              for (auto ff : zeroesAdd)
-                     merged.push_back(ff);
+        for (auto ff : onesAdd)
+            merged.push_back(ff);
 
-              for (auto ff : onesAdd)
-                     merged.push_back(ff);
+        return merged;
+    }
 
-              return merged;
-       }
 
-       vector<int> grayCode(int n) {
-              vector<int> ans;
+    vector<int> grayCode(int n) {
+        vector<int> ans;
 
-              string cur;
-              vector<string> ansStr = help(n);
+        vector<string> ansStr = help(n);
 
-              for (auto x : ansStr) {
-                     int y = stoi(x, 0, 2);
-                     ans.push_back(y);
-              }
+        for (auto x : ansStr) {
+            int y = stoi(x, 0, 2);
+            ans.push_back(y);
+        }
 
-              return ans;
-       }
+        return ans;
+    }
 };

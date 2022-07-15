b2b45c0449815646554bd3d6c3afceb3573464f7
Susnata Goswami <55017730+proghead00@users.noreply.github.com>
2022-07-16 02:54:13 +0530

    Time: 946 ms (18.72%), Space: 0B (100.00%) - LeetHub

--- 180-consecutive-numbers/180-consecutive-numbers.sql
+++ 180-consecutive-numbers/180-consecutive-numbers.sql
@@ -7,7 +7,7 @@ Logs l1, Logs l2, Logs l3
 
 WHERE
 
-l1.id = l2.id - 1 AND l2.id = l3.id - 1 
+l1.id = l2.id - 1 AND l2.id = l3.id - 1  # checking if they have consecutive ids
 AND
-l1.num = l2.num AND l2.num = l3.num
+l1.num = l2.num AND l2.num = l3.num # checking if the number is the same for all
 

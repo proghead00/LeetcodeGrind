7e629133052c36239869240ce70650429c12533a
Susnata Goswami <55017730+proghead00@users.noreply.github.com>
2022-07-17 01:57:00 +0530

    Time: 303 ms (92.00%), Space: 0B (100.00%) - LeetHub

--- 182-duplicate-emails/182-duplicate-emails.sql
+++ 182-duplicate-emails/182-duplicate-emails.sql
@@ -1,11 +1,6 @@
 # Write your MySQL query statement below
 
-# get the count of each email, and then filter them accordingly
-
-SELECT email FROM
-
-(SELECT email, COUNT(email) AS cnt
-FROM Person 
-GROUP BY email) AS tempTable
-
-WHERE cnt > 1
+SELECT email 
+FROM Person
+GROUP BY email
+HAVING COUNT(email) > 1

d786268901a07af6f8690b09aa3c7fc529e820c6
Susnata Goswami <55017730+proghead00@users.noreply.github.com>
2022-07-17 22:17:28 +0530

    Time: 532 ms (88.54%), Space: 0B (100.00%) - LeetHub

---
+++ 184-department-highest-salary/184-department-highest-salary.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+
+SELECT Department.name AS "Department", Employee.name AS "Employee", Employee.salary AS Salary
+FROM Employee JOIN Department ON Employee.departmentId = Department.id
+
+WHERE (departmentId, Salary) IN 
+
+(SELECT departmentId, MAX(salary) FROM Employee
+GROUP BY departmentId)

93284e5891e8a01383ea53aa2256c3d007af04aa
Susnata Goswami <55017730+proghead00@users.noreply.github.com>
2022-07-17 21:01:32 +0530

    Time: 626 ms (66.49%), Space: 89.8 MB (26.86%) - LeetHub

--- 452-minimum-number-of-arrows-to-burst-balloons/452-minimum-number-of-arrows-to-burst-balloons.cpp
+++ 452-minimum-number-of-arrows-to-burst-balloons/452-minimum-number-of-arrows-to-burst-balloons.cpp
@@ -1,15 +1,10 @@
 class Solution {
 public:
-  static bool compare(vector<int> &v1, vector<int> &v2) {
-    return v1[1] < v2[1];
-  }
-
   int findMinArrowShots(vector<vector<int>> &points) {
 
-    // sort(points.begin(), points.end(),
-    //      [&](vector<int> a, vector<int> b) { return a[1] < b[1]; });
+    sort(points.begin(), points.end(),
+         [](vector<int> &a, vector<int> &b) { return a[1] < b[1]; });
 
-    sort(points.begin(), points.end(), compare);
     int end = points[0][1];
     int ans = 1;
 

07f28b2fe17366ed90927011d84b591e818ef77f
Susnata Goswami <55017730+proghead00@users.noreply.github.com>
2022-07-19 13:31:08 +0530

    Time: 423 ms (5.47%), Space: 89.6 MB (5.13%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -1,35 +1,32 @@
 class Solution {
 public:
-    int help(vector<int> &coins, int target, int idx, vector<vector<int>> &dp) {
-        if (idx == 0) {
-            // check if the 0th coin can form the target
-            return (target % coins[0] == 0) ? target / coins[0] : 1e9;
+    int coinChange(vector<int> &coins, int target) {
+        vector<vector<int>> dp(target + 1, vector<int>(coins.size(), 0));
+
+        // form base case for bottom-up
+        for (int t = 0; t <= target; t++) {
+            if (t % coins[0] == 0) dp[t][0] = t / coins[0];
+            else dp[t][0] = 1e9;
         }
 
-        if (dp[target][idx] != -1) {
-            return dp[target][idx];
+        // loop for the changing states
+        for (int idx = 1; idx < coins.size(); idx++) {
+            for (int t = 0; t <= target; t++) {
+                // copy the recurrence and mutate accordingly
+
+                int pick = 1e9, skip;
+
+                skip = dp[t][idx - 1];
+
+                if (coins[idx] <= t)
+                    pick = 1 + dp[t - coins[idx]][idx];
+
+                dp[t][idx] = min(pick, skip);
+
+            }
         }
 
-        int pick = 1e9, skip = 0;
-        // pick = 1e9 (and not 0) ==> Why? Because, let's say,
-        // skip gives me value of 3 and pick doesn't form the target, so it gives 0
-        // the min is 0 which is wrong
-
-        skip = help(coins, target, idx - 1, dp);
-
-        if (coins[idx] <= target)
-
-            // pick that coin and move on for further calls (keep idx fixed since I can take infinite ones of that denomination)
-            pick = 1 + help(coins, target - coins[idx], idx, dp); // keeping idx as it is since I have infinite for the same
-
-
-        return dp[target][idx] = min(pick, skip);
-    }
-
-    int coinChange(vector<int> &coins, int amount) {
-        vector<vector<int>> dp(amount + 1, vector<int>(coins.size(), -1)); // amount ranges from 0 to amount (hence +1 for the size)
-
-        int ans = help(coins, amount, coins.size() - 1, dp);
+        int ans = dp [target][coins.size() - 1];
 
         if (ans >= 1e9) return -1;
         else return ans;

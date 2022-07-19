f0a562569d92832a50d81a89f85b6cfe48128e8f
Susnata Goswami <55017730+proghead00@users.noreply.github.com>
2022-07-19 12:30:15 +0530

    Time: 417 ms (5.57%), Space: 90.1 MB (5.13%) - LeetHub

---
+++ 322-coin-change/322-coin-change.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int help(vector<int> &coins, int target, int idx, vector<vector<int>> &dp) {
+        if (idx == 0) {
+            // check if the 0th coin can form the target
+            return (target % coins[0] == 0) ? target / coins[0] : 1e9;
+        }
+
+        if (dp[target][idx] != -1) {
+            return dp[target][idx];
+        }
+
+        int pick = 1e9, skip = 0;
+        // pick = 1e9 (and not 0) ==> Why? Because, let's say,
+        // skip gives me value of 3 and pick doesn't form the target, so it gives 0
+        // the min is 0 which is wrong
+
+        skip = help(coins, target, idx - 1, dp);
+
+        if (coins[idx] <= target)
+            pick = 1 + help(coins, target - coins[idx], idx, dp); // keeping idx as it is
+        // since I have infinite
+        // supply for the same
+
+        return dp[target][idx] = min(pick, skip);
+    }
+
+    int coinChange(vector<int> &coins, int amount) {
+        vector<vector<int>> dp(amount + 1, vector<int>(coins.size(), -1)); // amount ranges from 0 to amount (hence +1 for the size)
+
+        int ans = help(coins, amount, coins.size() - 1, dp);
+
+        if (ans >= 1e9) return -1;
+        else return ans;
+    }
+};

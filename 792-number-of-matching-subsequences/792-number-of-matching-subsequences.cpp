a0a64bbe5a1e34fbdf6349231ef39b759638829f
Susnata Goswami <55017730+proghead00@users.noreply.github.com>
2022-07-20 10:50:19 +0530

    Time: 269 ms (72.46%), Space: 38 MB (87.39%) - LeetHub

--- 792-number-of-matching-subsequences/792-number-of-matching-subsequences.cpp
+++ 792-number-of-matching-subsequences/792-number-of-matching-subsequences.cpp
@@ -3,7 +3,6 @@ public:
     bool check(const string &og, const string w) {
 
         // at max, I need to move thru the og string trying to find for the character
-        // O(og.size())
 
         // 2 pointers approach
         int i = 0, j = 0;
@@ -19,14 +18,13 @@ public:
     int numMatchingSubseq(string s, vector<string>& words) {
         unordered_map<string, int> mp;
 
-        // O(w)
         for (int i = 0; i < words.size(); i++) mp[words[i]]++;
 
         int ans = 0;
 
-        //  hashmap has all the unique words => O(words.size())  ==> O(w)
+        //  hashmap has all the unique words
+        // O(s * (w * L)) => w = no. of unique words, L = max length of word
 
-        // O(w * s)
         for (auto pp : mp)
             if (check(s, pp.first)) ans += pp.second; // for multiple occurences of words
 

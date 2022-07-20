c06cbf4eb465b6b18a6fbb11105a41bfc9779efd
Susnata Goswami <55017730+proghead00@users.noreply.github.com>
2022-07-20 12:25:48 +0530

    Time: 648 ms (25.60%), Space: 49.8 MB (54.00%) - LeetHub

--- 792-number-of-matching-subsequences/792-number-of-matching-subsequences.cpp
+++ 792-number-of-matching-subsequences/792-number-of-matching-subsequences.cpp
@@ -1,33 +1,33 @@
 class Solution {
 public:
-    bool check(const string &og, const string w) {
-
-        // at max, I need to move thru the og string trying to find for the character
-
-        // 2 pointers approach
-        int i = 0, j = 0;
-        while (i < og.size() and j < w.size()) {
-            if (og[i] == w[j]) i++, j++;
-            else i++;
-        }
-
-        if (j == w.size()) return true;
-        else return false;
-    }
 
     int numMatchingSubseq(string s, vector<string>& words) {
-        unordered_map<string, int> mp;
+        unordered_map<char, vector<int>> mp;
 
-        for (int i = 0; i < words.size(); i++) mp[words[i]]++;
+        for (int i = 0; i < s.size(); i++) {
+            mp[s[i]].push_back(i);
+        }
 
         int ans = 0;
 
-        //  hashmap has all the unique words
-        // O(s * (w * L)) => w = no. of unique words, L = max length of word
+        for (auto w : words) {
+            int prev = -1;
+            
+            for (int i = 0; i < w.size(); i++) {
 
-        for (auto pp : mp)
-            if (check(s, pp.first)) ans += pp.second; // for multiple occurences of words
+                if (mp.count(w[i]) == 0) {
+                    break; // if the character is not at all present in s, move on to the next string in the vector
+                }
+                int idx = upper_bound(mp[w[i]].begin(), mp[w[i]].end(), prev) - mp[w[i]].begin();
+
+                if (idx == mp[w[i]].size()) break;
+
+                prev = mp[w[i]][idx];
+                if (i == w.size() - 1) ans++;
+            }
+        }
 
         return ans;
     }
 };
+

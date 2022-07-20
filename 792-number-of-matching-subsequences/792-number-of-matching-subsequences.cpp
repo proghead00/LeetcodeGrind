71980ed4096f52759a803c045e03a586935e33d0
Susnata Goswami <55017730+proghead00@users.noreply.github.com>
2022-07-20 10:45:46 +0530

    Time: 295 ms (66.61%), Space: 37.8 MB (88.89%) - LeetHub

--- 792-number-of-matching-subsequences/792-number-of-matching-subsequences.cpp
+++ 792-number-of-matching-subsequences/792-number-of-matching-subsequences.cpp
@@ -2,8 +2,10 @@ class Solution {
 public:
     bool check(const string &og, const string w) {
 
-        // O(max(og, w)) => O(og)
+        // at max, I need to move thru the og string trying to find for the character
+        // O(og.size())
 
+        // 2 pointers approach
         int i = 0, j = 0;
         while (i < og.size() and j < w.size()) {
             if (og[i] == w[j]) i++, j++;
@@ -22,8 +24,10 @@ public:
 
         int ans = 0;
 
+        //  hashmap has all the unique words => O(words.size())  ==> O(w)
+
+        // O(w * s)
         for (auto pp : mp)
-            // O(w * max(s, w)) => O(w * s)
             if (check(s, pp.first)) ans += pp.second; // for multiple occurences of words
 
         return ans;

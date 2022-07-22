a79ca97f0657429995b8031862d3a67c742ce190
Susnata Goswami <55017730+proghead00@users.noreply.github.com>
2022-07-22 22:40:30 +0530

    Time: 3 ms (99.31%), Space: 8.8 MB (86.73%) - LeetHub

--- 684-redundant-connection/684-redundant-connection.cpp
+++ 684-redundant-connection/684-redundant-connection.cpp
@@ -1,33 +1,24 @@
 class Solution {
 public:
-    bool dfs(int cur, int par, vector<vector<int>>& gr, vector<bool>& vis) {
-        vis[cur] = 1;
-
-        for (auto nbr : gr[cur]) {
-            if (!vis[nbr]) {
-                if (dfs(nbr, cur, gr, vis)) return true;
-            }
-            else if (nbr != par)
-                return true;
-        }
-        return false;
+    int find(int node, vector<int>& par) {
+        if (node == par[node]) return node;
+        else return par[node] = find(par[node], par);
     }
 
-    vector<int> findRedundantConnection(vector<vector<int>>& edges) {
-        int n = edges.size();
-        vector<vector<int>> gr(n + 1);
-        vector<bool> vis(n + 1, 0);
+    vector<int> findRedundantConnection(vector<vector<int>>& es) {
+        vector<int> par(es.size() + 1);
 
-        for (auto e : edges) {
-            fill(begin(vis), end(vis), 0);
-            gr[e[0]].push_back(e[1]);
-            gr[e[1]].push_back(e[0]);
+        // initially every node is the parent of itself
+        for (int i = 1; i < es.size() + 1; i++)
+            par[i] = i;
 
-            if (dfs(e[0], -1, gr, vis))
-                return e;
+        for (auto &e : es) {
+            int ff = find(e[0], par);
+            int ss = find(e[1], par);
+
+            if (ff != ss) par[ff] = ss;
+            else return {e[0], e[1]};
         }
-
         return {};
     }
-
 };

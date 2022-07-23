68000f87c7b3c20caf5e87b633ccca33a43b1e64
Susnata Goswami <55017730+proghead00@users.noreply.github.com>
2022-07-23 13:29:44 +0530

    Time: 32 ms (49.73%), Space: 13.7 MB (29.98%) - LeetHub

--- 494-target-sum/494-target-sum.cpp
+++ 494-target-sum/494-target-sum.cpp
@@ -1,16 +1,38 @@
 class Solution {
 public:
-    int help(vector<int> &nums, int target, int sum, int idx) {
-        if (idx < 0) return target == sum;
 
-        int plus = help(nums, target, sum + nums[idx], idx - 1);
-        int minus = help(nums, target, sum - nums[idx], idx - 1);
+    vector<vector<int>> memo;
+    
+    int subsetsum(vector<int> &nums, int n, int target) {
 
-        return plus + minus;
+        if (!n) {
+            if (!target and nums[0] == 0) return 2; // when I have reached 0th pos, I can have 2 choices when 0 is present (pick or skip)
+
+            else if (!target or target == nums[0]) return 1;
+            else return 0;
+        }
+
+        if (memo[n][target] != -1)
+            return memo[n][target];
+
+        int nope = subsetsum(nums, n - 1, target);
+        int yes = 0;
+        if (nums[n] <= target) {
+            yes = subsetsum(nums, n - 1, target - nums[n]);
+        }
+
+        return memo[n][target] = nope + yes;
+
     }
 
     int findTargetSumWays(vector<int>& nums, int target) {
-        return help(nums, target, 0, nums.size() - 1);
+        int summ = accumulate(nums.begin(), nums.end(), 0);
+
+        int diff = summ - target;
+        if (diff & 1 or diff < 0) return 0;
+
+        int tar = diff / 2;
+        memo.resize(nums.size() + 10, vector<int>(tar + 10, -1));
+        return subsetsum(nums, nums.size() - 1, tar);
     }
 };
-

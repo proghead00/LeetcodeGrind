6087e652482e3299dc59372ae0f89a861e0b8893
Susnata Goswami <55017730+proghead00@users.noreply.github.com>
2022-07-25 14:39:29 +0530

    Time: 509 ms (5.20%), Space: 92.1 MB (5.00%) - LeetHub

--- 775-global-and-local-inversions/775-global-and-local-inversions.cpp
+++ 775-global-and-local-inversions/775-global-and-local-inversions.cpp
@@ -1,19 +1,52 @@
 class Solution {
 public:
-    bool isIdealPermutation(vector<int>& nums) {
-        // [1,0, 2] => global = 1, local = 1 (hence, return true)
-        // [2, 0, 1] => global = {2,0} {2,1}; local = {2,0}
-        //                  \U0001f449 i points at 0 (2), and i+2 is (1), and we find that 2 (i) > 1 (i+2) which violates local inversion property
+    long long int globalboi = 0;
+    long long int temp[1000000];
 
-        int n = nums.size();
-        int mx = 0;
-        for (int i = 0; i < n - 2; i++) {
-            mx = max(mx, nums[i]); // mx is the max number found so far, so if we get an element at i+2 which is less than mx, it forms global inv
+    void join(vector<int> &nums, int s, int e, int mid) {
+        long long int i = s, j = mid + 1;
+        long long int k = i;
 
-            if (mx > nums[i + 2])
-                return false;
+        while (i <= mid and j <= e) {
+
+            if (nums[i] <= nums[j]) {
+                temp[k++] = nums[i++];
+            }
+            else {
+                globalboi += mid - i + 1;
+                temp[k++] = nums[j++];
+            }
         }
 
-        return true;
+        while (i <= mid) temp[k++] = nums[i++];
+        while (j <= e) temp[k++] = nums[j++];
+
+        // copy back to nums
+        for (long long int k = s; k <= e; k++) nums[k] = temp[k];
+    }
+
+
+    void split(vector<int> &nums, int s, int e) {
+
+        if (s >= e) return;
+        int mid = s + (e - s) / 2;
+
+        split(nums, s, mid);
+        split(nums, mid + 1, e);
+
+        join(nums, s, e, mid);
+
+    }
+
+    bool isIdealPermutation(vector<int>& nums) {
+        if (nums.size() <= 1) return true;
+
+        int localboi = 0;
+        for (int i = 0; i < nums.size() - 1; i++)
+            if (nums[i] > nums[i + 1]) localboi++;
+
+        split(nums, 0, nums.size() - 1);
+
+        return globalboi == localboi;
     }
 };

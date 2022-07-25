bf036bf1a40722b1e2cb867c66adfc6c11a166e2
Susnata Goswami <55017730+proghead00@users.noreply.github.com>
2022-07-25 12:41:17 +0530

    Time: 213 ms (56.86%), Space: 83.3 MB (36.86%) - LeetHub

--- 775-global-and-local-inversions/775-global-and-local-inversions.cpp
+++ 775-global-and-local-inversions/775-global-and-local-inversions.cpp
@@ -1,79 +1,19 @@
-	class Solution {
+class Solution {
 public:
+    bool isIdealPermutation(vector<int>& nums) {
+        // [1,0, 2] => global = 1, local = 1 (hence, return true)
+        // [2, 0, 1] => global = {2,0} {2,1}; local = {2,0}
+        //                  \U0001f449 i points at 0 (2), and i+2 is (1), and we find that 2 (i) > 1 (i+2) which violates local inversion property
 
-//1 2 3  // 0 1 2
+        int n = nums.size();
+        int mx = 0;
+        for (int i = 0; i < n - 2; i++) {
+            mx = max(mx, nums[i]); // mx is the max number found so far, so if we get an element at i+2 which is less than mx, it forms global inv
 
-long long  merge(vector<int>&nums,vector<int>&temp,int low,int mid,int high){
-    
-    if(low>=high)
-        return 0;
-    
-    
-    long long res=0;
-    long long j=mid;
-    long long k=low;
-    long long i=low;
-    while(j<=high && i<mid){
-        
-        if(nums[i]<=nums[j]){
-            temp[k++]=nums[i++];
+            if (mx > nums[i + 2])
+                return false;
         }
-        else{
-            temp[k++]=nums[j++];
-            res+=(mid-i);
-        }
-    }
-    
-    while(i<mid){
-        temp[k++]=nums[i++];
-    }
-    
-    while(j<=high){
-        temp[k++]=nums[j++];
-    }
-    
-    
-    for (int m = low; m <= high; m++)
-	nums[m] = temp[m];
-    
-    
-    return res;
-}
 
-long long mergeSort(vector<int>&nums,vector<int>&temp,int low,int high){
-    
-    if(low>=high)
-        return 0;
-    
-    long long mid=low+(high-low)/2;
-    
-    long long left=mergeSort(nums,temp,low,mid);
-    long long  right=mergeSort(nums,temp,mid+1,high);
-    
-    return left+right+merge(nums,temp,low,mid+1,high);
-    
-}
-
-int countInversion(vector<int>&nums){
-    
-    vector<int>temp(nums.size());
-    return mergeSort(nums,temp,0,nums.size()-1);
-    
-}
-
-bool isIdealPermutation(vector<int>& nums) {
- 
-    int LI=0;
-    
-    for(int i=0;i<nums.size()-1;i++){
-        
-        if(nums[i]>nums[i+1])
-            LI++;
-        
+        return true;
     }
-    
-    int GI=countInversion(nums);
-    return GI==LI;
-    
-}
 };

668aea34b4838e17e24899ebcf24568ced84d0f2
Susnata Goswami <55017730+proghead00@users.noreply.github.com>
2022-07-30 20:28:09 +0530

    Time: 7 ms (99.72%), Space: 17 MB (74.76%) - LeetHub

--- 57-insert-interval/57-insert-interval.cpp
+++ 57-insert-interval/57-insert-interval.cpp
@@ -16,7 +16,7 @@ public:
         
         vector<int> temp = newInterval;
         
-        while(i < intervals.size() and intervals[i][0] <= newInterval[1]){
+        while(i < intervals.size() and newInterval[1] >= intervals[i][0] and intervals[i][1] >= newInterval[0]){
             temp[0] = min(temp[0], intervals[i][0]);
             temp[1] = max(temp[1], intervals[i][1]);
             i++;

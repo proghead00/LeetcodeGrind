b6b08550c4d9fae3f43be4ff8b5ae14219914240
Susnata Goswami <55017730+proghead00@users.noreply.github.com>
2022-07-30 17:08:28 +0530

    Time: 8 ms (81.44%), Space: 9.1 MB (70.73%) - LeetHub

--- 47-permutations-ii/47-permutations-ii.cpp
+++ 47-permutations-ii/47-permutations-ii.cpp
@@ -1,29 +1,23 @@
 class Solution {
 public:
-    void help(vector<int> &nums, vector<vector<int>> &ans, int idx) {
-        if (idx == nums.size()) {
-            ans.push_back(nums);
-            return;
-        }
-
-        vector<int> vis(21, 0);
-
-        for (int i = idx; i < nums.size(); i++) {
-            if (vis[nums[i] + 10] == 1)
-                continue;
-
-            vis[nums[i] + 10] = 1;
-
-            swap(nums[idx], nums[i]);
-            help(nums, ans, idx + 1);
-            swap(nums[idx], nums[i]);
-        }
+  void help(vector<int> nums, vector<vector<int>> &ans, int idx) {
+    if (idx == nums.size() - 1) {
+      ans.push_back(nums);
+      return;
     }
 
-    vector<vector<int>> permuteUnique(vector<int>& nums) {
-        vector<vector<int>> ans;
-        sort(nums.begin(), nums.end());
-        help(nums, ans, 0);
-        return ans;
+    for (int i = idx; i < nums.size(); i++) {
+      if (i!=idx and nums[i] == nums[idx]) continue;
+
+      swap(nums[idx], nums[i]);
+      help(nums, ans, idx + 1);
     }
+  }
+
+  vector<vector<int>> permuteUnique(vector<int>& nums) {
+    vector<vector<int>> ans;
+    sort(nums.begin(), nums.end());
+    help(nums, ans, 0);
+    return ans;
+  }
 };

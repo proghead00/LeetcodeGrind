ff266793573e5131fdad2f94466595faf0f540a0
Susnata Goswami <55017730+proghead00@users.noreply.github.com>
2022-07-30 20:04:05 +0530

    Time: 16 ms (87.09%), Space: 17.1 MB (74.76%) - LeetHub

--- 57-insert-interval/57-insert-interval.cpp
+++ 57-insert-interval/57-insert-interval.cpp
@@ -1,52 +1,34 @@
-// 3 types of insertions:
-
-// 1.  Push the ``intervals``` that come before ```newInterval``` when there is NO overlap
-
-// 2. There is overlap, so we gotta extend our range of ```mergeOverlaps``` as [minimum....maximum] values respectively
-
-// 3. After the merging is completed, there is no overlap henceforth. We can
-// just push the remaining last elements into ```ans```
-
-
 class Solution {
 public:
-  vector<vector<int>> insert(vector<vector<int>> &intervals, vector<int> &newInterval) {
-
-    int i = 0;
-    vector<vector<int>> ans;
-
-    int n = intervals.size();
-    // intervals input is already sorted
-
-    // 1.
-    while (i < n) {
-      if (intervals[i][1] >= newInterval[0])
-        break;
-      ans.push_back(intervals[i]);
-      i++;
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+        
+        vector<vector<int>> ans;
+        
+        // non overlaps
+        int i = 0;
+        while(i < intervals.size() and intervals[i][1] < newInterval[0]){
+            ans.push_back(intervals[i]);
+            i++;
+        }
+        
+        // i will point to the next vector 
+        // overlap case
+        
+        vector<int> temp = newInterval;
+        
+        while(i < intervals.size() and intervals[i][0] <= newInterval[1]){
+            temp[0] = min(temp[0], intervals[i][0]);
+            temp[1] = max(temp[1], intervals[i][1]);
+            i++;
+        }
+        
+        ans.push_back(temp);
+        
+        while(i < intervals.size()){
+            ans.push_back(intervals[i]); 
+            i++; 
+        }
+        
+        return ans;
     }
-
-    // 2.
-    vector<int> mergeOverlaps = newInterval;
-    while (i < n) {
-
-      // if there's no overlap
-      if (intervals[i][0] > newInterval[1])
-        break;
-
-      // we need to expand the mergeOverlaps according to min and max
-      mergeOverlaps[0] = min(mergeOverlaps[0], intervals[i][0]);
-      mergeOverlaps[1] = max(mergeOverlaps[1], intervals[i][1]);
-      i++;
-    }
-
-    ans.push_back(mergeOverlaps);
-
-    // 3.
-    while (i < n) {
-      ans.push_back(intervals[i]);
-      i++;
-    }
-    return ans;
-  }
 };

ba7919ffeda37839f8b07698b80e50ad7d7d9b10
Susnata Goswami <55017730+proghead00@users.noreply.github.com>
2022-07-30 17:09:41 +0530

    Time: 17 ms (50.98%), Space: 9.4 MB (52.66%) - LeetHub

--- 47-permutations-ii/47-permutations-ii.cpp
+++ 47-permutations-ii/47-permutations-ii.cpp
@@ -7,7 +7,7 @@ public:
     }
 
     for (int i = idx; i < nums.size(); i++) {
-      if (i!=idx and nums[i] == nums[idx]) continue; // NB: nums[idx] is compared with nums[idx]
+      if (i!=idx and nums[i] == nums[idx]) continue; // NB: nums[idx] is compared with nums[i]
       swap(nums[idx], nums[i]);
       help(nums, ans, idx + 1);
     }

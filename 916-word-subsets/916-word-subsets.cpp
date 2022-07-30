33d0bc418ff85e7c07a08c0d6383cd8cf3e0a281
Susnata Goswami <55017730+proghead00@users.noreply.github.com>
2022-07-30 13:40:25 +0530

    Time: 405 ms (32.59%), Space: 102.4 MB (65.17%) - LeetHub

--- 916-word-subsets/916-word-subsets.cpp
+++ 916-word-subsets/916-word-subsets.cpp
@@ -1,27 +1,39 @@
+ 
 class Solution {
 public:
-  vector<string> wordSubsets(vector<string> &words1, vector<string> &words2) {
-    vector<string> result;
-    vector<int> counts(26, 0);
-    
-    for (auto &word : words2) {
-      vector<int> counts2(26, 0);
-      for (auto &c : word)
-        counts2[c - 'a']++;
-      for (int i = 0; i < 26; i++)
-        counts[i] = max(counts[i], counts2[i]);
+    vector<string> wordSubsets(vector<string> &words1, vector<string> &words2) {
+      
+        vector<string> ans;
+        vector<int> map2(26, 0);
+
+        for (string &w : words2) {
+            vector<int> temp2(26, 0);
+
+            for (char &c : w)
+                temp2[c - 'a']++;
+
+            // need to store the max frequencies for each character
+            for (int i = 0; i < 26; i++)
+                map2[i] = max(map2[i], temp2[i]);
+        }
+
+
+        for (auto &w : words1) {
+
+            vector<int> temp1(26, 0);
+            for (auto &c : w)
+                temp1[c - 'a']++;
+
+            bool ok = true;
+            for (int i = 0; i < 26; i++)
+                if (temp1[i] < map2[i])
+                    ok = false;
+
+            if (ok)
+                ans.push_back(w);
+        }
+        
+        return ans;
     }
-    for (auto &word : words1) {
-      vector<int> counts2(26, 0);
-      for (auto &c : word)
-        counts2[c - 'a']++;
-      bool ok = true;
-      for (int i = 0; i < 26; i++)
-        if (counts2[i] < counts[i])
-          ok = false;
-      if (ok)
-        result.push_back(word);
-    }
-    return result;
-  }
 };
+ 

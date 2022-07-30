87c79246a947b1951ccbab82b494e9be6a0bfeb1
Susnata Goswami <55017730+proghead00@users.noreply.github.com>
2022-07-30 15:47:23 +0530

    Time: 12 ms (12.40%), Space: 7.7 MB (54.57%) - LeetHub

--- 46-permutations/46-permutations.cpp
+++ 46-permutations/46-permutations.cpp
@@ -1,28 +1,36 @@
 class Solution {
 public:
-    vector<vector<int>> ans;
-    void help(vector<int> &nums, vector<int> &vis, vector<int> &temp) {
-        if (temp.size() == nums.size()) {
+    
+    void help(vector<int> &nums, vector<int> &temp, vector<vector<int>> &ans, unordered_map<int, int> &mp){
+        
+        if(temp.size()==nums.size()) {
             ans.push_back(temp);
             return;
         }
-
-        for (int i = 0; i < nums.size(); i++) {
-            if (vis[i]) continue;
-
+        
+        for(int i=0; i<nums.size(); i++){
+            if(mp[nums[i]] == 1) continue;
+            
+            mp[nums[i]] = 1;
+            
             temp.push_back(nums[i]);
-            vis[i] = 1;
-            help(nums, vis, temp);
-
+            
+            help(nums, temp, ans, mp);
+            
+            // backtrack
+            mp[nums[i]] = 0;
             temp.pop_back();
-            vis[i] = 0;
         }
     }
-
+    
     vector<vector<int>> permute(vector<int>& nums) {
-        vector<int> vis(22, 0);
+        
+        vector<vector<int>> ans;
+        unordered_map<int, int> mp;
         vector<int> temp;
-        help(nums, vis, temp);
+        
+        help(nums, temp, ans, mp);
+        
         return ans;
     }
 };

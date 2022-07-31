6a8d92b75d07399e1fc9b7f7d5e71d5e4a9b5a77
Susnata Goswami <55017730+proghead00@users.noreply.github.com>
2022-08-01 00:58:00 +0530

    Time: 182 ms (70.79%), Space: 49.7 MB (50.75%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
@@ -1,20 +1,24 @@
 class Solution {
 public:
-  int longestConsecutive(vector<int> &nums) {
-    unordered_set<int> s(nums.begin(), nums.end());
-    int ans = 0;
-    // We gotta find the starting point of the consecutive sequence.
-    for (int x : nums) {
-      // If there exists a number which is one less than x, we don't care, cuz x
-      // is not the starting point here
-      if (!s.count(x - 1)) {
-        // if x is the starting point, we need to find the end point.
-        int y = x + 1;
-        while (s.count(y))
-          y++;
-        ans = max(ans, y - x);
-      }
+    int longestConsecutive(vector<int>& nums) {
+        
+        if(nums.size() == 0) return 0;
+        
+        unordered_map<int, int> mp;
+        for(int x: nums) mp[x]++;
+        
+        int mx = 1, c = 1;
+        
+        for(int x: nums){
+            
+            if(mp.count(x-1)) continue;
+            
+            while(mp.count(x+1)) x++, c++, mp.erase(x);
+            
+            mx = max(c, mx);
+            c = 1;
+        } 
+        
+        return mx;
     }
-    return ans;
-  }
 };

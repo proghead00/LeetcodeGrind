ee43134d2734d830ca1d979eb45bbc76c63915e8
Susnata Goswami <55017730+proghead00@users.noreply.github.com>
2022-08-01 10:12:54 +0530

    Time: 212 ms (52.34%), Space: 136.3 MB (25.87%) - LeetHub

--- 131-palindrome-partitioning/131-palindrome-partitioning.cpp
+++ 131-palindrome-partitioning/131-palindrome-partitioning.cpp
@@ -1,36 +1,38 @@
 class Solution {
 public:
-  bool isPal(string &s, int start, int end) {
-    while (start < end) {
-      if (s[start] != s[end])
-        return false;
-      start++;
-      end--;
-    }
-    return true;
-  }
-
-  void help(string &s, int idx, vector<string> temp,
-            vector<vector<string>> &ans) {
-    if (idx == s.size()) {
-      ans.push_back(temp);
-      return;
-    }
-
-    for (int i = idx; i < s.size(); i++) {
-      if (isPal(s, idx, i)) {
-        temp.push_back(s.substr(idx, i - idx + 1));
-        help(s, i + 1, temp, ans);
-        temp.pop_back();
-      }
-      
-    }
-  }
     
-  vector<vector<string>> partition(string s) {
-    vector<vector<string>> ans;
-    vector<string> temp;
-    help(s, 0, temp, ans);
-    return ans;
-  }
+    bool isPal(string &s, int i, int j){
+
+        while(i <= j){
+            if(s[i] != s[j]) return false;
+            i++, j--;
+        }
+        
+        return true;
+    }
+    
+    void help(string &s, vector<string> temp, vector<vector<string>> &ans, int idx){
+    
+        if(idx == s.size()){
+            ans.push_back(temp);
+            return;
+        }
+        
+        for(int i = idx ; i < s.size(); i++){
+            if(isPal(s, idx, i)) {
+                temp.push_back(s.substr(idx, i - idx + 1));
+                help(s, temp, ans, i + 1);
+                temp.pop_back();
+            }
+        }
+            
+    }
+    
+    vector<vector<string>> partition(string s) {
+        vector<string> temp;
+        vector<vector<string>> ans;
+        help(s, temp, ans, 0);
+        
+        return ans;
+    }
 };

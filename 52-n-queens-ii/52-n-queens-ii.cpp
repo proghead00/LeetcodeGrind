64c6797d3f7ad6f3cf76fa68d011f168eb5e111e
Susnata Goswami <55017730+proghead00@users.noreply.github.com>
2022-08-02 12:36:41 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (97.43%) - LeetHub

--- 52-n-queens-ii/52-n-queens-ii.cpp
+++ 52-n-queens-ii/52-n-queens-ii.cpp
@@ -1,32 +1,35 @@
+
 class Solution {
 public:
-  int ans = 0;
+    int ans = 0;
 
-  void help(int n, bitset<9> &rows, bitset<18> &ld,
-            bitset<18> &rd, int c) {
-    if (c == n) {
-      ans++;
-      return;
+    void help(int n, bitset<9> &rows, bitset<18> &ld, bitset<18> &rd, int c) {
+        if (c == n) {
+            ans++;
+            return;
+        }
+
+        for (int r = 0; r < n; r++) {
+            if (!rows[r] and !ld[r + c] and !rd[c - r + n - 1]) {
+
+                rows[r] = ld[r + c] = rd[c - r + n - 1] = 1;
+
+                help(n, rows, ld, rd, c + 1);
+
+                // backtrack
+                rows[r] =  ld[r + c] = rd[c - r + n - 1] = 0;
+            }
+        }
     }
 
-    for (int r = 0; r < n; r++) {
-      if (rows[r] == 0 and ld[r + c] == 0 and rd[c - r + n - 1] == 0) {
-        rows[r] = 1;
-        ld[r + c] = 1;
-        rd[c - r + n - 1] = 1;
-        help(n, rows, ld, rd, c + 1);
+    int totalNQueens(int n) {
 
-        rows[r] = 0;
-        ld[r + c] = 0;
-        rd[c - r + n - 1] = 0;
-      }
+        bitset<9> rows(0);
+        bitset<18> ld(0);
+        bitset<18> rd(0);
+
+        help(n, rows, ld, rd, 0);
+        return ans;
     }
-  }
-  int totalNQueens(int n) {
-    bitset<9> rows(0);
-    bitset<18> ld(0);
-    bitset<18> rd(0);
-    help(n, rows, ld, rd, 0);
-    return ans;
-  }
 };
+

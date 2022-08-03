fe5121514d4b005187bf7791bdb8d290002610b9
Susnata Goswami <55017730+proghead00@users.noreply.github.com>
2022-08-03 12:54:47 +0530

    Time: 39 ms (67.71%), Space: 6.4 MB (49.84%) - LeetHub

--- 37-sudoku-solver/37-sudoku-solver.cpp
+++ 37-sudoku-solver/37-sudoku-solver.cpp
@@ -1,47 +1,68 @@
 class Solution {
 public:
-  bool valid(vector<vector<char>> &board, int r, int c, char choice) {
 
-    for (int i = 0; i < 9; i++) {
+    bool valid(vector<vector<char>> &board, int r, int c, char choice) {
 
-      if (board[i][c] == choice)
-        return false;
+        for (int i = 0; i < 9; i++) {
 
-      if (board[r][i] == choice)
-        return false;
-
-      int rrr = 3 * (r / 3) + i / 3;
-      int ccc = 3 * (c / 3) + i % 3;
-
-      if (board[rrr][ccc] == choice)
-        return false;
-    }
-
-    return true;
-  }
-
-    
-  bool fill(vector<vector<char>> &board) {
-    for (int r = 0; r < board.size(); r++) {
-      for (int c = 0; c < board[0].size(); c++) {
-
-        if (board[r][c] == '.') {
-          for (char choice = '1'; choice <= '9'; choice++) {
-
-            if (valid(board, r, c, choice)) {
-              board[r][c] = choice;
-              if (fill(board))
-                return true;
-              else
-                board[r][c] = '.';
+            // if the same no. is present in that col
+            if (board[i][c] == choice) {
+                return false;
             }
-          }
-          return false;
+
+            // if present in that row
+            if (board[r][i] == choice) {
+                return false;
+            }
+
+            // if present in the subgrid
+            int rrr = 3 * (r / 3) + i / 3;
+            int ccc = 3 * (c / 3) + i % 3;
+
+            if (board[rrr][ccc] == choice)
+                return false;
         }
-      }
+
+        return true;
     }
-    return true;
-  }
-    
-  void solveSudoku(vector<vector<char>> &board) { fill(board); }
+
+    bool fill(vector<vector<char>> &board) {
+        // try to place 1-9
+
+        // search for a blank spot to be filled up
+        for (int r = 0; r < board.size(); r++) {
+            for (int c = 0; c < board[0].size(); c++) {
+
+                // blank spot found
+                if (board[r][c] == '.') {
+
+                    // try to fill
+                    for (char choice = '1'; choice <= '9'; choice++) {
+
+                        if (valid(board, r, c, choice)) { // if I can fill (for the time)
+
+                            // fill it
+                            board[r][c] = choice;
+
+                            // now ask recursion to temme if it's a correct fill, and whether there's no issue in the "future"
+                            if (fill(board))
+                                return true;
+                            else
+                                // backtrack, so remove that char and try to fill other char
+                                board[r][c] = '.';
+                        }
+                    }
+
+                    // everything's filled up and matrix is traversed
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    void solveSudoku(vector<vector<char>>& board) {
+        fill(board);
+    }
 };

5184ea4da9e14c73b5a4c613f9b8943d38f79101
Susnata Goswami <55017730+proghead00@users.noreply.github.com>
2022-08-05 17:59:49 +0530

    Time: 14 ms (29.12%), Space: 7.3 MB (57.63%) - LeetHub

--- 467-unique-substrings-in-wraparound-string/467-unique-substrings-in-wraparound-string.cpp
+++ 467-unique-substrings-in-wraparound-string/467-unique-substrings-in-wraparound-string.cpp
@@ -1,16 +1,24 @@
 class Solution {
 public:
+    /*
+    SUBSTRING IS CONTIGUOUS
 
+    Input: abcdedefgh
+    abcd --> length = 4
+
+    The mistake which I made was: I did a streak reset to 1 when I found that a character is repeating, here, it's 'd' at the 5th position
+    At the 5th position, we find another 'd' but we MUST NOT reset streak since we might form a different substring later on
+
+    */
     int findSubstringInWraproundString(string p) {
         unordered_map<char, int> mp;
+
+        // if the characters are not contiguous and also check whether after 'z' I am getting 'a'. If so, reset the streak to 1
+        // else streak++
 
         int streak = 0;
 
         for (int i = 0; i < p.size(); i++) {
-
-            // duplicate found
-            // DONT DO THIS:
-            // if (mp.count(p[i])) streak = 1;
 
             // contiguous
             if (i and (p[i] - p[i - 1] == 1)) streak++;
@@ -23,6 +31,7 @@ public:
             mp[p[i]] = max(mp[p[i]], streak);
         }
 
+        // why sum? cuz there might be "single" substrings when the streak's broken
 
         int ans = 0;
         for (auto x : mp) {
@@ -32,3 +41,4 @@ public:
         return ans;
     }
 };
+

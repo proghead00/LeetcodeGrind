3a70158b21a15c8269c37a451010f73d64b0b2b6
Susnata Goswami <55017730+proghead00@users.noreply.github.com>
2022-08-07 16:13:32 +0530

    Time: 449 ms (25.00%), Space: 83.7 MB (100.00%) - LeetHub

--- 2364-count-number-of-bad-pairs/2364-count-number-of-bad-pairs.cpp
+++ 2364-count-number-of-bad-pairs/2364-count-number-of-bad-pairs.cpp
@@ -1,23 +1,26 @@
 class Solution {
 public:
-    long long countBadPairs(vector<int>& nums) {
-        
-        /*
-        j - i != nums[j] - nums[i]
-        j - nums[j] != i - nums[i]
-         
-        [..., 4] -> Now, I got != as the condition, so I need to find if there are
-                    j - 4  before this
-        */
-        
-        unordered_map<int, int> mp;
-   
-        long long ans = 0;
-        for(int i = 0; i < nums.size(); i++){
-            ans += i - mp[i - nums[i]];
-            mp[i - nums[i]]++;
-        }
-        
-        return ans;
+  long long countBadPairs(vector<int>& nums) {
+
+    /*
+       j - i != nums[j] - nums[i]
+    => j - nums[j] != i - nums[i]
+
+    [..., 4] -> Since I got != as the condition, so I need to find if there are other numbers that are not equal to the current number.
+          |
+          i           i means that we have explored i numbers before (since i starts from 0)
+
+
+    */
+
+    unordered_map<int, int> mp;
+
+    long long ans = 0;
+    for (int i = 0; i < nums.size(); i++) {
+      ans += i - mp[i - nums[i]]; // taking upto i, so I need to subtract the number of times we have seen the number before
+      mp[i - nums[i]]++;
     }
+
+    return ans;
+  }
 };

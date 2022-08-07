dcab16ce9186261aa370fef8a9bb3574dcff7ee0
Susnata Goswami <55017730+proghead00@users.noreply.github.com>
2022-08-07 15:58:24 +0530

    Time: 246 ms (100.00%), Space: 83.8 MB (100.00%) - LeetHub

--- 2364-count-number-of-bad-pairs/2364-count-number-of-bad-pairs.cpp
+++ 2364-count-number-of-bad-pairs/2364-count-number-of-bad-pairs.cpp
@@ -1,15 +1,23 @@
 class Solution {
 public:
-    long long countBadPairs(vector<int>& a) {
-    
-     long long cnt = 0;
-     unordered_map<int,int> mp;
+    long long countBadPairs(vector<int>& nums) {
+        
+        /*
+        j - i != nums[j] - nums[i]
+        j - nums[j] != i - nums[i]
+         
+        [..., 4] -> Now, I got != as the condition, so I need to find if there are
+                    j - 4  before this
+        */
+        
+        unordered_map<int, int> mp;
    
-     for(int i=0;i<size(a);i++){
-        cnt+= i- mp[i-a[i]];
-        mp[i-a[i]]++;
+        long long ans = 0;
+        for(int i = 0; i < nums.size(); i++){
+            ans += i - mp[i - nums[i]];
+            mp[i - nums[i]]++;
+        }
+        
+        return ans;
     }
-     
-    return cnt;
-  }
 };

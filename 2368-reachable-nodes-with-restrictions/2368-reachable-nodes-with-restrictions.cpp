698fc6ffc9c588c6823a837dbc857b2424ec4de8
Susnata Goswami <55017730+proghead00@users.noreply.github.com>
2022-08-07 09:53:04 +0530

    Time: 762 ms (22.22%), Space: 167.2 MB (11.11%) - LeetHub

---
+++ 2368-reachable-nodes-with-restrictions/2368-reachable-nodes-with-restrictions.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+  vector<int> gr[100005];
+  int vis[100005];
+
+  int help(int cur, int par) {
+    if (cur == -1) return 0;
+    int cnt = 1;
+
+    vis[cur] = 1;
+    for (auto &x : gr[cur]) {
+      if (x == par or vis[x] == 1) continue;
+      cnt += + help(x, cur);
+    }
+
+    return cnt;
+  }
+
+  int reachableNodes(int n, vector<vector<int>> &edges, vector<int> &restricted) {
+
+    for (auto &g : gr)
+      g.clear();
+
+    memset(vis, 0, sizeof(vis));
+
+    for (auto pp : restricted) {
+      vis[pp] = 1;
+    }
+
+    for (auto x : edges) {
+      if (vis[x[0]] or vis[x[1]]) continue;
+        gr[x[0]].push_back(x[1]);
+        gr[x[1]].push_back(x[0]);
+      }
+
+    return help(0, -1);
+  }
+};

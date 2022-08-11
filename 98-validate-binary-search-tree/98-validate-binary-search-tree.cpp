28f62088d7c733756eeb12b91461f92b4324e22d
Susnata Goswami <55017730+proghead00@users.noreply.github.com>
2022-08-11 12:36:19 +0530

    Time: 17 ms (65.02%), Space: 21.5 MB (98.63%) - LeetHub

--- 98-validate-binary-search-tree/98-validate-binary-search-tree.cpp
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.cpp
@@ -1,5 +1,7 @@
 class Solution {
 public:
+  // Constraint:-2 ^ 31 <= Node.val <= 2 ^ 31 - 1. Therefore take LLONG_MAX and LLONG_MIN
+
   bool help(TreeNode* root, long long mx, long long mn) {
     if (!root) return true;
 

7e116f4b5c9074ae7847b6034e99baf1e720b847
Susnata Goswami <55017730+proghead00@users.noreply.github.com>
2022-08-12 08:14:10 +0530

    Time: 56 ms (28.61%), Space: 23.1 MB (91.99%) - LeetHub

--- 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.cpp
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -1,17 +1,11 @@
 class Solution {
 public:
   TreeNode *lowestCommonAncestor(TreeNode *root, TreeNode *p, TreeNode *q) {
-
-    if (!root or root == p or root == q)
-      return root;
-
-    TreeNode* left = lowestCommonAncestor(root->left, p, q);
-    TreeNode* right = lowestCommonAncestor(root->right, p, q);
-
-    if (!left) return right;
-    else if (!right) return left;
+   
+    // find the split
+    if(p->val < root->val  and q->val < root->val) return lowestCommonAncestor(root->left, p, q);
+    else if(p->val > root->val  and q->val > root->val) return lowestCommonAncestor(root->right, p, q);
     else return root;
-
 
   }
 };

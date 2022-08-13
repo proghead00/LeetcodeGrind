6007dedd35d737eb315cd8ad7a43b363cb0464e1
Susnata Goswami <55017730+proghead00@users.noreply.github.com>
2022-08-13 19:32:43 +0530

    Time: 108 ms (42.78%), Space: 9.4 MB (78.51%) - LeetHub

--- 44-wildcard-matching/44-wildcard-matching.cpp
+++ 44-wildcard-matching/44-wildcard-matching.cpp
@@ -1,61 +1,53 @@
 class Solution {
 public:
+
   bool isMatch(string s, string p) {
 
-    int patSize = p.size();
     int ogSize = s.size();
+    int patSize = p.size();
 
-    vector<vector<bool>> dp(patSize + 1, vector<bool>(ogSize + 1, false));
+    vector<vector<bool>> dp(patSize + 1, vector<bool> (ogSize + 1, false));
 
-    // i --> idx_p (patSize)
-    // j --> idx_s (ogSize)
 
-    // we can fix * in p and check for other characters in s, hence
-    // outer-looping wrt p
+    // we can fix * in p and check for other characters in s, hence outer looping wrt p
+    // i --> idx_p
+    // j --> idx_s
 
-    //------------------------------------------------------
-    // 1)  base case:
+    for (int i = 0; i <= patSize; i++) {
+      for (int j = 0; j <= ogSize; j++) {
 
-    // i==0, j==0 --> true
-    dp[0][0] = true;
-
-    // i==0 and j>0 --> false [j is non-zero so start from 1]
-    for (int j = 1; j <= ogSize; j++) {
-      // put i=0 in dp[i][j]
-      dp[0][j] = false;
-    }
-
-    // i>0 and j==0 [i is non-zero so start from 1]
-    for (int i = 1; i <= patSize; i++) {
-      bool f = true;
-      for (int k = 1; k <= i; k++) {
-        if (p[k - 1] != '*') {
-          f = false;
-          break;
+        if (j == 0 and i == 0) {
+          dp[i][j] = true;
         }
-      }
-      // put j=0 in dp[i][j]
-      dp[i][0] = f;
-    }
-    //------------------------------------------------------
 
-    // 2)  recursive case:
+        else if (i == 0 and j)  {
+          dp[i][j] = false;
+        }
 
-    for (int i = 1; i <= patSize; i++) {
-      for (int j = 1; j <= ogSize; j++) {
-
-        if (s[j - 1] == p[i - 1] or p[i - 1] == '?')
-          dp[i][j] = dp[i - 1][j - 1];
+        else if (j == 0 and i) {
+          bool f = true;
+          for (int kk = 1; kk <= i; kk++) {
+            if (p[kk - 1] != '*') {
+              f = false;
+              break;
+            }
+          }
+          dp[i][j] = f;
+        }
 
         else {
-          if (p[i - 1] == '*')
+
+          if (p[i - 1] == s[j - 1] or p[i - 1] == '?') dp[i][j] = dp[i - 1][j - 1];
+
+          else if (p[i - 1] == '*') {
             dp[i][j] = dp[i - 1][j] or dp[i][j - 1];
-          else
-            dp[i][j] = false;
+          }
+          else dp[i][j] = false;
         }
       }
     }
 
     return dp[patSize][ogSize];
+
   }
 };

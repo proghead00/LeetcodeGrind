346b902cf6d88418d8dbc8311e3a67d2bc390639
Susnata Goswami <55017730+proghead00@users.noreply.github.com>
2022-08-13 13:00:53 +0530

    Time: 2126 ms (5.00%), Space: 24.2 MB (46.75%) - LeetHub

--- 44-wildcard-matching/44-wildcard-matching.cpp
+++ 44-wildcard-matching/44-wildcard-matching.cpp
@@ -1,40 +1,42 @@
 class Solution {
 public:
 
-  // int dp[2001][2001];
+  int dp[2001][2001];
 
-  bool help(string &s, string &p, int idx_s, int idx_p, vector<vector<int>> &dp) {
+  bool help(string &s, string &p, int idx_s, int idx_p) {
 
     // base conditions:
-    if (idx_p == 0 and idx_s == 0) return true;
+    if (idx_p < 0 and idx_s < 0) return true;
 
-    if (idx_s > 0 and idx_p == 0) return false; // string's left, but there's no pattern left
+    if (idx_s >= 0 and idx_p < 0) return false; // string's left, but there's no pattern left
 
-    if (idx_s == 0 and idx_p > 0) {
-      for (int i = 1; i <= idx_p; i++) if (p[i - 1] != '*') return false; // since only * can match with empty sequence
+    if (idx_s < 0 and idx_p >= 0) {
+      for (int i = 0; i <= idx_p; i++) if (p[i] != '*') return false; // since only * can match with empty sequence
 
       //NB: I missed out to return true previusly, hence -ve index threw obvious errors:
       return true;
     }
 
 
-    if (dp[idx_s][idx_p] != -1) return dp[idx_s][idx_p];
+    if (dp[idx_s][idx_p] != -1) {
+      if (dp[idx_s][idx_p] == 1) return true;
+      else return false;
+    }
 
 
     // recurrence:
 
     // chars match
-    if (s[idx_s - 1] == p[idx_p - 1] or p[idx_p - 1] == '?') return dp[idx_s][idx_p] = help(s, p, idx_s - 1, idx_p - 1, dp);
+    if (s[idx_s] == p[idx_p] or p[idx_p] == '?') return dp[idx_s][idx_p] = help(s, p, idx_s - 1, idx_p - 1);
 
 
     // * is encountered
-    if (p[idx_p - 1] == '*') {
-
+    if (p[idx_p] == '*') {
       // option 1: use * as nothing
-      bool op1 = help(s, p, idx_s, idx_p - 1, dp);
+      bool op1 = help(s, p, idx_s, idx_p - 1);
 
       // option 2: use * as a matching ==> fix * and move the other string
-      bool op2 = help(s, p, idx_s - 1, idx_p, dp);
+      bool op2 = help(s, p, idx_s - 1, idx_p);
 
       return dp[idx_s][idx_p] = op1 or op2;
     }
@@ -50,9 +52,12 @@ public:
 
     // memset(dp, -1, sizeof(dp));
 
-    vector<vector<int>> dp(s.size() + 1, vector<int> (p.size() + 1, -1));
-
-    return help(s, p, s.size(), p.size(), dp);
+    for (int i = 0; i < s.size(); i++) {
+      for (int j = 0; j < p.size(); j++)
+        dp[i][j] = -1;
+    }
+    
+    return help(s, p, s.size() - 1, p.size() - 1);
 
   }
 };

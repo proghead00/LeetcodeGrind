76785759910ea7bfab92448ae7133a64eaf00a2d
Susnata Goswami <55017730+proghead00@users.noreply.github.com>
2022-08-22 10:54:44 +0530

    Time: 8 ms (8.64%), Space: 6 MB (24.97%) - LeetHub

--- 342-power-of-four/342-power-of-four.cpp
+++ 342-power-of-four/342-power-of-four.cpp
@@ -1,14 +1,19 @@
 class Solution {
 public:
     bool isPowerOfFour(int n) {
-        
-        if(n == 0) return 0;
-        if(n == 1) return 1;
-        
-        bool sub1 = !(n % 4);
-        bool sub2 = isPowerOfFour(n / 4);
-        
-        return sub1 & sub2;
-        
+        // gotta 2 do checks:
+        // 1. no. of set bits in n should be equal to 1 (pwoer of 2 concept)
+        // 2. the bit that is set, should be in the even position (based on lop variable i here)
+
+        if (__builtin_popcount(n) > 1) return false;
+
+        int cnt = 0;
+        for (int i = 0; i < 32; i++) {
+            if (((n >> i) & 1) and (i % 2 == 0)) {
+                cnt++;
+                if (cnt > 1) return false;
+            }
+        }
+        return (cnt == 1) ? true : false;
     }
 };

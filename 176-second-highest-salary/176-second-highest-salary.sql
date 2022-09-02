bc01f9616e00ef9572398129cb1b020370f0a508
Susnata Goswami <55017730+proghead00@users.noreply.github.com>
2022-09-02 19:29:05 +0530

    Time: 1112 ms (5.00%), Space: 0B (100.00%) - LeetHub

--- 176-second-highest-salary/176-second-highest-salary.sql
+++ 176-second-highest-salary/176-second-highest-salary.sql
@@ -1,11 +1,17 @@
 # Write your MySQL query statement below
 
 SELECT IFNULL(
-    NULL,
+    (
+    SELECT DISTINCT salary  
+    FROM Employee e1
+    WHERE 1 = (
+              SELECT COUNT(DISTINCT salary) # there should not be space b/w count and the parenthesis
+              FROM Employee e2
+              WHERE e1.salary < e2.salary
+              )
+    ),
     
-    (SELECT DISTINCT salary
-    FROM Employee
-    ORDER BY salary DESC
-    LIMIT 1 OFFSET 1))
-    
-    AS SecondHighestSalary # AS --> in the end 
+    NULL
+)
+              
+AS SecondHighestSalary 

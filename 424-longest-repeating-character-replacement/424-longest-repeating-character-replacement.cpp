067e2a1c736edcecfc76c99dc6d1f91d20eb0c43
Susnata Goswami <55017730+proghead00@users.noreply.github.com>
2022-09-26 11:10:10 +0530

    Time: 25 ms (38.96%), Space: 7.1 MB (52.11%) - LeetHub

--- 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.cpp
+++ 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.cpp
@@ -1,41 +1,28 @@
 class Solution {
 public:
     int characterReplacement(string s, int k) {
-        // we move the window and check for windowLength - maxFreq <= k everytime
-        // windowLength - maxFreq is the no. of characters we need to replace
-
+      
         unordered_map<char, int> mp;
-
-        int l = 0, r = 0;
-        int mxFreq = 0;
-        int windowLength = 0;
-        int ans = windowLength;
-
-        while (r < s.size()) {
-
+        int mx = 0, windowLen;
+        
+        int l = 0, r = 0, ans = 0;
+        
+        while(r < s.size()){
             mp[s[r]]++;
-
-            windowLength = r - l + 1; // window will continue until replacements cross k
-
-            // at every step I increase r, so rightwise it increases
-            // I will increase l when I find windowLength - mxFreq crosses k
-
-            mxFreq = max(mxFreq, mp[s[r]]);
-
-            if (windowLength - mxFreq <= k) {
-                // no need to move l
-                ans = max(ans, windowLength);
-            }
-            else {
-                // need to move l when it crosses k
-                mp[s[l]]--; // since I am shifting l, I need to omit the presence of the current character at l
+            mx = max(mx, mp[s[r]]);
+            windowLen = r - l + 1;
+            
+            if(windowLen - mx > k) {
+                mp[s[l]]--;
                 l++;
-
             }
-            // r will always move
+            else{
+                ans = max(ans, windowLen);
+            }
             r++;
         }
-
+        
         return ans;
+        
     }
 };

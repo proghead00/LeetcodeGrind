b78f0b1d2f3974dec05297e9acbec9811faef5fe
Susnata Goswami <55017730+proghead00@users.noreply.github.com>
2022-10-08 16:58:11 +0530

    Time: 3 ms (74.89%), Space: 5.9 MB (24.79%) - LeetHub

--- 69-sqrtx/69-sqrtx.cpp
+++ 69-sqrtx/69-sqrtx.cpp
@@ -6,19 +6,15 @@ public:
         while (l <= h) {
             long long mid = l + (h - l) / 2;
 
-            if (mid * mid == x) {
+            if (mid * mid == x)  
                 return mid;
-            }
 
-            else if (mid * mid < x) {
+            else if (mid * mid < x)  
                 l = mid + 1;
-            }
-
-            else {
-                h = mid - 1;
-            }
+       
+            else h = mid - 1;
         }
+        
         return ans;
-
     }
 };

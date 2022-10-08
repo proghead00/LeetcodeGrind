13931f157d79616613578adc0cfab0f253202dac
Susnata Goswami <55017730+proghead00@users.noreply.github.com>
2022-10-08 16:57:36 +0530

    Time: 4 ms (58.90%), Space: 5.8 MB (73.24%) - LeetHub

---
+++ 69-sqrtx/69-sqrtx.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        int l = 0, h = x, ans;
+
+        while (l <= h) {
+            long long mid = l + (h - l) / 2;
+
+            if (mid * mid == x) {
+                return mid;
+            }
+
+            else if (mid * mid < x) {
+                l = mid + 1;
+            }
+
+            else {
+                h = mid - 1;
+            }
+        }
+        return ans;
+
+    }
+};

56e748a2adcf86920d10b168fe6498b19d61f818
Susnata Goswami <55017730+proghead00@users.noreply.github.com>
2022-10-09 21:19:48 +0530

    Time: 30 ms (59.75%), Space: 18.5 MB (99.41%) - LeetHub

---
+++ 275-h-index-ii/275-h-index-ii.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int hIndex(vector<int>& citations) {
+        int n = citations.size();
+        int l = 0, h = n - 1;
+
+        while (l <= h) {
+            int mid = l + (h - l) / 2;
+            if (citations[mid] == n - mid) return citations[mid];
+            else if (citations[mid] < n - mid) l = mid + 1;
+            else h = mid - 1;
+        }
+
+        return n - l;
+    }
+};
+
+

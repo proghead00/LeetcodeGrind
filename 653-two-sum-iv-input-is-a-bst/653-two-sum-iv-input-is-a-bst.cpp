2fff95f451862cc0184f5a64b4a2b48c2b2748b0
Susnata Goswami <55017730+proghead00@users.noreply.github.com>
2022-10-09 21:40:12 +0530

    Time: 138 ms (5.07%), Space: 41.7 MB (8.97%) - LeetHub

--- 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.cpp
+++ 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.cpp
@@ -3,9 +3,17 @@ class Solution {
 public:
     bool findTarget(TreeNode* root, int k) {
         if (!root) return false;
+
+        // if the remaining number is already present
         if (mp.count(k - root->val)) return true;
+
+        // map the current node's value
         mp[root->val]++;
-        return findTarget(root->left, k) or findTarget(root->right, k);
+
+        bool left = findTarget(root->left, k);
+        bool right = findTarget(root->right, k);
+
+        return left or right;
     }
 
 };

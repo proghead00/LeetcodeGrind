ee1db727692becc13c7770d64d5a830a1c2f0370
Susnata Goswami <55017730+proghead00@users.noreply.github.com>
2022-10-09 21:22:20 +0530

    Time: 38 ms (39.15%), Space: 18.6 MB (48.31%) - LeetHub

--- 275-h-index-ii/275-h-index-ii.cpp
+++ 275-h-index-ii/275-h-index-ii.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int hIndex(vector<int>& citations) {
         int n = citations.size();
-        int l = 0, h = n - 1; // h = n - 1 (not h = n)
+        int l = 0, h = n - 1; // h = n - 1 (not h = n since max could be the last element in the array: l and h denote indices)
 
         while (l <= h) {
             int mid = l + (h - l) / 2;

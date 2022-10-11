f1375c36b73a15061e5dca5ebf376050db3d2ec1
Susnata Goswami <55017730+proghead00@users.noreply.github.com>
2022-10-11 19:29:37 +0530

    Time: 486 ms (16.67%), Space: 46.4 MB (43.76%) - LeetHub

---
+++ 1901-find-a-peak-element-ii/1901-find-a-peak-element-ii.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    vector<int> findPeakGrid(vector<vector<int>>& mat) {
+
+        // NB: The perimeter of -1 is just an assumption
+        int l = 0, h = mat.size() - 1;
+
+        while (l <= h) {
+            int mid = l + (h - l) / 2;
+
+            // left and right adjacent checks are handled by this
+            int idx, mx = 0;
+            for (int i = 0; i < mat[0].size(); i++) {
+                if (mat[mid][i] > mx) {
+                    mx = mat[mid][i];
+                    idx = i;
+                }
+            }
+
+            // handling top and bottom now according to different cases of indices
+            if (mid == 0) {
+                if (mat[0][idx] > mat[1][idx])
+                    return {0, idx};
+                // else
+                //     return {1, idx};
+            }
+
+            else if (mid == mat.size() - 1) {
+                if (mat[mid][idx] > mat[mid - 1][idx])
+                    return {mid, idx};
+                // else
+                //     return {mid - 1, idx};
+            }
+
+            else if (mat[mid][idx] > mat[mid + 1][idx] and mat[mid][idx] > mat[mid - 1][idx])
+                return {mid, idx};
+
+
+            // truncate search space: generic BS
+            if (mat[mid][idx] < mat[mid + 1][idx]) l = mid + 1;
+            else h = mid - 1;
+
+        }
+
+        return {0, 0};
+    }
+};

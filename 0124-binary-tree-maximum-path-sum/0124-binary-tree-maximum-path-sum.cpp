4b2f696a6ed635a40aa44b67e9c6442d6a669e20
Susnata Goswami <55017730+proghead00@users.noreply.github.com>
2022-10-20 20:08:19 +0530

    Time: 24 ms (96.01%), Space: 27.8 MB (36.62%) - LeetHub

---
+++ 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int mx = INT_MIN;
+    
+    int help(TreeNode* root) {
+        
+        if(!root) return 0;
+        
+        // take max values from left and right subtrees
+        int left = max(0, help(root->left));
+        int right = max(0, help(root->right));
+        
+        // we already have the max values from left and right subtrees
+        mx = max(mx, left + root->val + right);
+        
+        // now return the best path to the parent 
+        return max(left, right) + root->val;
+    }
+    
+    int maxPathSum(TreeNode* root) {
+        // int mx = INT_MIN;
+        help(root);
+        return mx;
+    }
+};

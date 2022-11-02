0081b47eabc3068b69f50937fffd5b38c2c121b9
Susnata Goswami <55017730+proghead00@users.noreply.github.com>
2022-11-02 22:46:33 +0530

    Time: 5 ms (75.36%), Space: 6.6 MB (68.81%) - LeetHub

--- 0763-partition-labels/0763-partition-labels.cpp
+++ 0763-partition-labels/0763-partition-labels.cpp
@@ -7,14 +7,12 @@ public:
         for (int i = 0; i < n; i++)
             lastIdx[s[i] - 'a'] = i;
 
-
         int start = 0;
         int end = 0;
 
         vector<int> ans;
 
         for (int i = 0; i < n; i++) {
-            // start++;
             end = max(end, lastIdx[s[i] - 'a']);
             if (i == end) {
                 ans.push_back(end - start + 1);
